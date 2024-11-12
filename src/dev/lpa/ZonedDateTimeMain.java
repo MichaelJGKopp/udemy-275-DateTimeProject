bf60054a133f8ec209df871f932a5e81b2e54cd6
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-12 14:54:06 +0100

    show ZoneIds and rules for Australia

--- src/dev/lpa/ZonedDateTimeMain.java
+++ src/dev/lpa/ZonedDateTimeMain.java
@@ -1,9 +1,18 @@
 package dev.lpa;
 
+import java.time.ZoneId;
+
 public class ZonedDateTimeMain {
   
   public static void main(String[] args) {
-  
-  
+    
+    System.out.println(ZoneId.systemDefault());
+    
+    System.out.println("Number of TZs = " + ZoneId.getAvailableZoneIds().size());
+    ZoneId.getAvailableZoneIds().stream()
+      .filter(s -> s.startsWith("Australia"))
+      .sorted()
+      .map(ZoneId::of)
+      .forEach(z -> System.out.println(z.getId() + ": " + z.getRules()));
   }
 }
