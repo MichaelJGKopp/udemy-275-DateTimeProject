435b0dfa79a5b4a84d0aac36a7accf6e7c6a66ff
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-12 14:04:37 +0100

    show LocalTime and LocalDateTime

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,8 +1,8 @@
 package dev.lpa;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Period;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 
@@ -72,5 +72,50 @@ public class Main {
     May5.datesUntil(May5.plusDays(7)).forEach(System.out::println); // stream incl./excl.
     System.out.println("-------------------------------------------------");
     May5.datesUntil(May5.plusYears(1), Period.ofDays(7)).forEach(System.out::println);
+    System.out.println();
+    
+    LocalTime time = LocalTime.now();
+    System.out.println(time);
+    
+    LocalTime sevenAM = LocalTime.of(7, 0);
+    System.out.println(sevenAM);
+    
+    LocalTime sevenThirty = LocalTime.of(7, 30, 15);
+    System.out.println(sevenThirty);
+    
+    LocalTime sevenPM = LocalTime.parse("11:59"); // "7:00" can not be parsed, "07:00" can
+    LocalTime sevenThirtyPM = LocalTime.parse("19:30:15.1000");
+    System.out.println(sevenPM.get(ChronoField.AMPM_OF_DAY)); // 0 -> AM
+    System.out.println(sevenThirtyPM.get(ChronoField.AMPM_OF_DAY)); // 1 -> PM
+    System.out.println(sevenThirtyPM.getHour());
+    System.out.println(sevenThirtyPM.get(ChronoField.HOUR_OF_DAY));
+    
+    // System.out.println(sevenThirtyPM.get(ChronoField.YEAR)); // not possible for LocalTime
+    
+    System.out.println(sevenThirtyPM.plus(24, ChronoUnit.HOURS));
+    
+    System.out.println(sevenPM.range(ChronoField.HOUR_OF_DAY));
+    System.out.println(sevenPM.range(ChronoField.MINUTE_OF_HOUR));
+    System.out.println(sevenPM.range(ChronoField.MINUTE_OF_DAY));
+    System.out.println(sevenPM.range(ChronoField.SECOND_OF_MINUTE));
+    System.out.println(sevenPM.range(ChronoField.SECOND_OF_DAY));
+    
+    LocalDateTime todayAndNow = LocalDateTime.now();
+    System.out.println(todayAndNow);
+    
+    LocalDateTime May5Noon = LocalDateTime.of(2022, 5, 5, 12, 0);
+    
+    System.out.printf("%tD %tr %n", May5Noon, May5Noon);
+    System.out.printf("%1$tF %1$tT %n", May5Noon);
+    
+    System.out.println(todayAndNow.format(DateTimeFormatter.ISO_WEEK_DATE));
+    
+    DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+    System.out.println(May5Noon.format(dtf));
+    
+    System.out.println(May5Noon.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+    
+    LocalDateTime May6Noon = May5Noon.plusHours(24);
+    System.out.println(May6Noon.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
   }
 }
